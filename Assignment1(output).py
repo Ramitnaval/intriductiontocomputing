ecf90d191cb00f334d423519dc22640e56f7bfe0
Ramitnaval <104322484+Ramitnaval@users.noreply.github.com>
2022-04-24 12:07:08 -0700

    Add files via upload

---
+++ Assignment1(output).py
@@ -0,0 +1,37 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: C:/Users/laptop/Desktop/1.py ====================
+enter first number:6
+enter first number:8
+enter first number:10
+avg 8.0
+
+===================== RESTART: C:/Users/laptop/Desktop/2.py ====================
+Enter your gross income200000
+Enter number of dependents6
+Tax: 34400.0
+
+===================== RESTART: C:/Users/laptop/Desktop/3.py ====================
+Enter your SID : 21102128
+Enter your Name : Ramit Naval
+Enter your Gender : M
+Enter your Coursename : Introduction to Computing
+Enter your CGPA : 10
+['21102128', 'Ramit Naval', 'M', 'Introduction to Computing', '10']
+
+===================== RESTART: C:/Users/laptop/Desktop/4.py ====================
+Marks of 1st student: 60
+Marks of 2nd student: 90
+Marks of 3rd student: 50
+Marks of 4th student: 30
+Marks of 5th student: 20
+[60, 90, 50, 30, 20]
+
+==================== RESTART: C:/Users/laptop/Desktop/5A.py ====================
+List of colors before removing:  ['Red', 'Green', 'White', 'Black', 'Pink', 'yellow']
+your list after removing 4th element:  ['Red', 'Green', 'White', 'Pink', 'yellow']
+
+==================== RESTART: C:/Users/laptop/Desktop/5B.py ====================
+List of colors before replacing: ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+your list after replacing black and pink with purple: ['Red', 'Green', 'White', 'Purple', 'Yellow']
---
+++ Assignment1.py
@@ -0,0 +1,54 @@
+# Q.1
+a = int(input("enter first number:"))
+b = int(input("enter first number:"))
+c = int(input("enter first number:"))
+
+avg = (a+b+c)/3
+
+print("avg",avg)
+
+
+# Q.2
+
+gross_income = float(input("Enter your gross income"))
+number_of_dependents = float(input("Enter number of dependents"))
+std_deduction = 10000
+tax_income = gross_income - std_deduction -(number_of_dependents * 3000)
+tax = (tax_income * 20)/100
+if(tax<0):
+    print("Invalid input")
+else:
+    print("Tax:", tax)
+
+# Q.3
+SID = input("Enter your SID : ")
+Name = input("Enter your Name : ")
+Gender = input("Enter your Gender : ")
+Coursename = input("Enter your Coursename : ")
+CGPA = input("Enter your CGPA : ")
+student = [SID, Name, Gender, Coursename, CGPA]
+print(student)
+
+# Q.4
+m1 = int(input("Marks of 1st student: "))
+m2 = int(input("Marks of 2nd student: "))
+m3 = int(input("Marks of 3rd student: "))
+m4 = int(input("Marks of 4th student: "))
+m5 = int(input("Marks of 5th student: "))
+SortedMarks = [m1, m2, m3, m4, m5]
+print(SortedMarks)
+
+#Q5(a)
+
+List_of_colors = ['Red', 'Green', 'White', 'Black', 'Pink', 'yellow']
+print("List of colors before removing: ", List_of_colors)
+List_of_colors.pop(3)
+print("your list after removing 4th element: ",List_of_colors)
+
+#Q5(b)
+
+List_of_colors = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+print("List of colors before replacing:",List_of_colors)
+List_of_colors[3:5]= ['Purple']
+print("your list after replacing black and pink with purple:", List_of_colors)
+
