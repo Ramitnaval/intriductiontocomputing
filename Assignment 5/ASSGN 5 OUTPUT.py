130379ef747816ac873dad525b85be780be6d321
Ramitnaval <104322484+Ramitnaval@users.noreply.github.com>
2022-06-18 08:11:33 -0700

    Add files via upload

---
+++ Assignment 5/ASSGN 5 OUTPUT.py
@@ -0,0 +1,106 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: C:/Users/laptop/Desktop/assgn 5 inputQ.py ==============
+ANS.1
+Enter the the string :SMARTPHONE
+ENOHPTRAMS
+ANS.2
+Enter the minimum value :2
+Enter the maximum value: 89
+Enter the number for divisibility check :6
+6
+12
+18
+24
+30
+36
+42
+48
+54
+60
+66
+72
+78
+84
+ANS.3
+Enter the length of side:5
+Enter the length of side:7
+Enter the length of side:8
+17.320508075688775
+ANS.4
+Enter the no. of rows :9
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+ANS.5
+Enter the number of rows5
+
+A
+BC
+DEF
+GHIJ
+KLMNO
+ANS.6
+Enter the Lowest Range Value : 1
+Enter the Upper Range Value : 87
+The Prime Numbers in this range are : 
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+ANS.7
+The numbers divisible by 11 and are multiple of 7 are : [77, 154, 231, 308, 385, 462]
+ANS.8
+Enter the number 9
+Enter the number 87
+Enter the number -98
+Enter the number 34
+Enter the number 26
+Enter the number -98
+Enter the number 56
+Enter the number 56
+Enter the number 13
+Enter the number 19
+Positive numbers are:  [9, 87, 34, 26, 56, 56, 13, 19]
+Negative numbers are:  [-98, -98]
+Even numbers are:  [-98, 34, 26, -98, 56, 56]
+Odd numbers are:  [9, 87, 13, 19]
+Number of times each number occurs in the List {9: 1, 87: 1, -98: 2, 34: 1, 26: 1, 56: 2, 13: 1, 19: 1}
+ANS.9
+Number of words: 9
+Enter the word: SKIP
+Enter the word: HELLO
+Enter the word: HELLO
+Enter the word: SKY
+Enter the word: SLEEP
+Enter the word: WALK
+Enter the word: OPEN
+Enter the word: FLAG
+Enter the word: FLOP
+Number of occurences:  {'SKIP': 1, 'HELLO': 2, 'SKY': 1, 'SLEEP': 1, 'WALK': 1, 'OPEN': 1, 'FLAG': 1, 'FLOP': 1}
