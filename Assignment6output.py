3d9e76aefd3ebcc63d163a002d8d6696fbe25c1b
Ramitnaval <104322484+Ramitnaval@users.noreply.github.com>
2022-07-03 09:57:02 -0700

    Create Assignment6output.py

---
+++ Assignment6output.py
@@ -0,0 +1,45 @@
+#question 1
+Write a number : 28
+True
+
+#question 2
+SPLIT TILPS
+YES, its palindrome
+
+#question 3
+[1]
+[1, 1]
+[1, 2, 1]
+[1, 3, 3, 1]
+[1, 4, 6, 4, 1]
+[1, 5, 10, 10, 5, 1]
+
+#question 4
+Sentence: the quick brown fox jumps over the lazy dog
+True
+
+#question 5
+green-red-yellow-black-white
+black-green-red-white-yellow
+
+#question 6
+name : RAMIT ||| branch : CIVIL
+--> RAMIT
+--> CIVIL
+
+#question 7
+True
+False
+True
+False
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+
+#question 8
+-10 2 8
+-7 -3 10
+
+#question 9
+enter : {[{[()]}]}
+{[{[()]}]} - is balanced
